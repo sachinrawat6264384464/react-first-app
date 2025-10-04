4a91446890ad28638e9e59aee94cdb9f8b6b184a
website <sacinrawat6264384464@gmail.com>
2025-10-04 09:58:51 +0530

    working onn aaap.js and login.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ function App() {
   return (
   <>
  <nav className="nav">
-     <h1> Hello web</h1>
+     <div className="navicon"><h1> |||</h1></div>
        <menu className='menu'>
            <h2>Home</h2>
            <h2>about</h2>
            <h2>contact</h2>
            <h2>Service</h2>
        </menu>
+       <div className="btns">
+       <button className="btn1"> SIGNUP  </button>
+       <button className="btn2"> SIGN-IN  </button>
+       </div>
  </nav>
  < Login/>
    </>
--- src/login.jsx
+++ src/login.jsx
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 import "./login.css"; // styling file
 
 function Login() {
-
+ const [Username,setUsername] = useState("")
+ const [password,setpassword] = useState("")
+  const onsubmitform =(e)=>{
+     e.preventDefault();
+      alert(`Email: ${Username}\npassword : ${password}`);
+ 
+ }
   return (
+  <>
+  <section className="home">
     <div className="login-container">
-      <h2>Login</h2>
-      <form className="login-form">
+      <h2>Login yourself</h2>
+      <form onSubmit={onsubmitform}  className="login-form">
         <div className="form-group">
-          <label>Email</label>
+          <label>Username</label>
           <input
             type="email"
-            placeholder="Enter your email"
-         
-          
+            placeholder="Enter your Username"
+           value={Username}
+           onChange={(e)=> setUsername(e.target.value)}
+           required
           
           />
         </div>
@@ -23,13 +32,20 @@ function Login() {
           <input
             type="password"
             placeholder="Enter your password"
-            
+              value={password}
+        onChange={(e)=> setpassword(e.target.value)}
+           required
           />
         </div>
 
         <button type="submit" className="btn">Login</button>
       </form>
     </div>
+    <div className="src">
+    <input type="search" placeholder="searching" className="inpu"/>
+      <button className="search">Search </button>                                                                                                                                                                                                                                                                                   
+    </div>
+    </section></>
   );
 }
 

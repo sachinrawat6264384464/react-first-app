3d45f73e2a3924152440c2e13d615452bc6fdb3a
website <sacinrawat6264384464@gmail.com>
2025-10-16 23:08:12 +0530

    sdgsa

--- src/component/nav.js
+++ src/component/nav.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="nav">
       <h3>
         VISIT_THE <span className="logo">INDIA</span>
       </h3>
 
-      <div className="menu">
-        <Link to="/" className="nav-link">Home</Link>
-        <Link to="/about" className="nav-link">About</Link>
-        <Link to="/favorites" className="nav-link">Favorites</Link>
-        
+      {/* Hamburger Button */}
+      <div className={`hamburger ${menuOpen ? "active" : ""}`} onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Menu Links */}
+      <div className={`menu ${menuOpen ? "open" : ""}`}>
+        <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}>About</Link>
+        <Link to="/favorites" className="nav-link" onClick={() => setMenuOpen(false)}>Favorites</Link>
       </div>
     </nav>
   );
